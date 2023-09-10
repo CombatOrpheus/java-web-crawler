e4cbb425ac8ba5071c30d97128553d1cad5c72a4
Gabriel Santos Oliveira <gsoliveira@localhost.localdomain>
2023-09-10 19:05:22 -0300

    main method skeleton

---
+++ src/main/java/com/webcrawler/backend/Main.java
@@ -0,0 +1,37 @@
+package com.webcrawler.backend;
+
+import static spark.Spark.get;
+import static spark.Spark.post;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.webcrawler.backend.crawler.Crawler;
+import com.webcrawler.backend.json.JsonHandler;
+import com.webcrawler.backend.repository.QueryRepository;
+
+import spark.Request;
+import spark.Response;
+
+public class Main {
+	public static void main(String[] args) {
+		post("/crawl", (Request req, Response res) -> {
+			String keyword = JsonHandler.getKeyword(req.body());
+			Optional<String> id = Crawler.validateKeyword(keyword);
+
+			res.type("application/json");
+			res.status(id.isPresent() ? 200 : 400);
+
+			return id.map(JsonHandler::toPostResponse).orElseGet(() -> "Invalid ID");
+		});
+
+		get("/crawl/:id", (req, res) -> {
+			String id = req.params("id");
+			Optional<List<String>> result = QueryRepository.getById(id);
+
+			res.type("application/json");
+			res.status(result.isPresent() ? 200 : 400);
+			return result.map(JsonHandler::toGetResponse).orElseGet(() -> "Unknow ID");
+		});
+	}
+}
