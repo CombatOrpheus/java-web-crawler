0684c6ee8cfe9d12f449def03fe2aaccd89ea54b
Gabriel Santos Oliveira <gsoliveira@localhost.localdomain>
2023-09-12 19:08:24 -0300

    some comments on the stream operations and documentation for classes and methods

--- src/main/java/com/webcrawler/backend/search/DownloadProcess.java
+++ src/main/java/com/webcrawler/backend/search/DownloadProcess.java
@@ -8,49 +8,80 @@ import java.util.Set;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.regex.MatchResult;
 import java.util.regex.Pattern;
+import java.util.stream.Stream;
 
-public class DownloadProcess {
+/**
+ * The main download process, which asynchronously and recursively searches for
+ * all the valid links in the HTML page. The process ends when all of the
+ * possible links are visited. The main entry point is the {@link this#crawl()}
+ * function.
+ */
+public final class DownloadProcess {
+	// TODO Implement logging
 
+	/**
+	 * An Environment variable that contains the root URL for the search process.
+	 */
 	private static final String BASE_URL = System.getenv("BASE_URL");
 
-	private static final Queue<Page> DOWNLOAD_QUEUE = new ConcurrentLinkedQueue<Page>();
+	/**
+	 * A {@link Queue} containing the links to new valid pages.
+	 */
+	private static final Queue<Page> SEARCH_QUEUE = new ConcurrentLinkedQueue<Page>();
+	/**
+	 * Pages that have been downloaded are kept in memory and made available for the
+	 * {@link SearchProcess} so that they are only downloaded once.
+	 */
 	private static final List<Page> DOWNLOADED_PAGES = new ArrayList<>();
+	/**
+	 * Every visited page is kept in this {@link Set} so as to avoid multiple visits
+	 * to the same URL.
+	 */
 	private static final Set<String> VISITED_PAGES = new HashSet<>();
-	
+
+	/**
+	 * A {@link Pattern} for the detection of anchor elements on the HTML pages.
+	 */
 	private static final Pattern ANCHOR = Pattern.compile("<a.+?>");
 
-	record Context(String url, String newUrl) {}
-	
+	record Context(String url, String newUrl) {
+	}
+
+	/**
+	 * The main method for the search, validation and download of HTML pages. It
+	 * remains active s long as there are elements in the {@link this#SEARCH_QUEUE}.
+	 * The processing is done with a {@link Stream}, which has additional comments
+	 * for clarity.
+	 */
 	public static void crawl() {
-		DOWNLOAD_QUEUE.add(new Page(BASE_URL));
+		SEARCH_QUEUE.add(new Page(BASE_URL));
 		do {
-			Page page = DOWNLOAD_QUEUE.poll();
-			DOWNLOAD_QUEUE.add(page);
+			Page page = SEARCH_QUEUE.poll();
+			SEARCH_QUEUE.add(page);
 			String contents = page.contents();
-			ANCHOR.matcher(contents)                               // Search for the HTML elements in a page
-					.results()                                     // Get a Stream with the regex matches
-					.map(MatchResult::group)                       // Get the matching Strings
-					.filter(SearchUtils::containsHref)             // Remove the elements that do not contain the field href
-					.map(SearchUtils::extractHref)                 // Extract the link contained in the href
-					.filter(SearchUtils::validLinks)               // Validate the links
-					.filter(String::isEmpty)
-					.map(link -> new Context(page.url(), link))    // Create a pair with the current page and the new link
-					.map(SearchUtils::handleLinks)                 // Handle relative links
+			ANCHOR.matcher(contents) // Search for the HTML elements in a page
+					.results() // Get a Stream with the regex matches
+					.map(MatchResult::group) // Get the matching Strings
+					.filter(SearchUtils::containsHref) // Remove the elements that do not contain the field href
+					.map(SearchUtils::extractHref) // Extract the link contained in the href
+					.filter(SearchUtils::validLinks) // Validate the links
+					.map(link -> new Context(page.url(), link)) // Create a pair with the current page and the new link
+					.map(SearchUtils::handleLinks) // Handle relative links
 					.filter(link -> !VISITED_PAGES.contains(link)) // Remove the links that we have already visited
-					.map(Page::new)                                // Create a new object and start the download process
-					.forEach(DOWNLOAD_QUEUE::add);
-			
-		} while (!DOWNLOAD_QUEUE.isEmpty());
+					.map(Page::new) // Create a new object and start the download process
+					.forEach(SEARCH_QUEUE::add);
+
+		} while (!SEARCH_QUEUE.isEmpty());
 	}
 
 	public static boolean isComplete() {
-		return DOWNLOAD_QUEUE.isEmpty();
+		return SEARCH_QUEUE.isEmpty();
 	}
-	
+
 	public static List<Page> getDownloadedPages() {
 		return DOWNLOADED_PAGES;
 	}
-	
+
 	public static Set<String> getVisitedPages() {
 		return VISITED_PAGES;
 	}
--- src/main/java/com/webcrawler/backend/search/SearchProcess.java
+++ src/main/java/com/webcrawler/backend/search/SearchProcess.java
@@ -10,12 +10,21 @@ import java.util.regex.Pattern;
 import com.webcrawler.backend.json.GetResponse;
 import com.webcrawler.backend.json.Status;
 
+/**
+ * This class implements the search process for a particular keyword and is
+ * created by {@link SearchScheduler}. The only method that has a return is the
+ * {@link SearchProcess#getResult() getResult()} method..
+ */
 class SearchProcess {
-	
+
 	private final String keyword;
 	private final List<String> results = new ArrayList<>();
 	private final Set<String> searchedPages = new HashSet<>();
 	private boolean complete;
+	/**
+	 * This is a {@link Predicate} created from the supplied keyword in order to
+	 * speed up the search process.
+	 */
 	private final Predicate<String> SEARCH_PATTERN;
 
 	SearchProcess(String keyword) {
@@ -23,21 +32,25 @@ class SearchProcess {
 		this.complete = false;
 		this.SEARCH_PATTERN = Pattern.compile(keyword).asMatchPredicate();
 	}
-	
+
+	/**
+	 * The main search process, which runs in a loop for as long as there are new
+	 * pages to search. It only completes when the {@link DownloadProcess} is done.
+	 */
 	public void start() {
 		do {
 			List<Page> pageList = DownloadProcess.getDownloadedPages();
-			for (Page page: pageList) {
+			for (Page page : pageList) {
 				String url = page.url();
 				String contents = page.contents();
-				
+
 				searchedPages.add(url);
 				boolean containsKeyword = SEARCH_PATTERN.test(contents);
 				if (containsKeyword) {
 					results.add(url);
 				}
 			}
-			
+
 			if (DownloadProcess.getVisitedPages().containsAll(searchedPages)) {
 				this.complete = true;
 			}
