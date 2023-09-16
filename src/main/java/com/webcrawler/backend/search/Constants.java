a114384e00d87ce0e9aad1da5f1d5d59c737cb84
Gabriel Santos Oliveira <gsoliveira@localhost.localdomain>
2023-09-16 16:53:37 -0300

    moving program contants into a single static class

---
+++ src/main/java/com/webcrawler/backend/search/Constants.java
@@ -0,0 +1,10 @@
+package com.webcrawler.backend.search;
+
+public final class Constants {
+	public static final String BASE_URL = System.getenv("BASE_URL");
+	/**
+	 * A configurable parameter for setting the length of the generated ID. If the
+	 * environment variable is not set, a default length of 10 is used.
+	 */
+	public static final int LENGTH_ID = Integer.parseInt(System.getenv("LENGTH_ID"));
+}
