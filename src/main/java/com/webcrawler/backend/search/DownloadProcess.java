db854618a7bccf6930ee90c70e6779efc9443c7f
Gabriel Oliveira <santosgabriel28@hotmail.com>
2024-01-06 15:01:49 -0300

    formatting changes

--- src/main/java/com/webcrawler/backend/search/DownloadProcess.java
+++ src/main/java/com/webcrawler/backend/search/DownloadProcess.java
@@ -1,13 +1,18 @@
 package com.webcrawler.backend.search;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Queue;
+import java.util.Set;
+import java.util.TreeSet;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.regex.MatchResult;
 import java.util.regex.Pattern;
 import java.util.stream.Stream;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * The main download process, which asynchronously and recursively searches for
@@ -18,7 +23,11 @@ import java.util.stream.Stream;
 public final class DownloadProcess implements Runnable {
 	private final String baseUrl;
 
+	private static final String HREF = "href=\"";
+
 	private static final Logger logger = LoggerFactory.getLogger(DownloadProcess.class);
+
+	private final HttpDownloader downloader = new HttpDownloader();
 
 	/**
 	 * A {@link Queue} containing the links to new valid pages.
@@ -30,8 +39,8 @@ public final class DownloadProcess implements Runnable {
 	 */
 	private static final List<Page> DOWNLOADED_PAGES = new ArrayList<>();
 	/**
-	 * Every visited page is kept in this {@link Set} to avoid multiple visits
-	 * to the same URL.
+	 * Every visited page is kept in this {@link Set} to avoid multiple visits to
+	 * the same URL.
 	 */
 	private static final Set<String> VISITED_PAGES = new TreeSet<>();
 
@@ -47,30 +56,24 @@ public final class DownloadProcess implements Runnable {
 	/**
 	 * The main method for the search, validation and download of HTML pages. It
 	 * remains active s long as there are elements in an internal search queue. The
-	 * processing is done with a {@link Stream}, which has additional comments for
-	 * clarity.
+	 * processing is done with a {@link Stream}.
 	 */
 	@Override
 	public void run() {
-		SEARCH_QUEUE.add(new Page(baseUrl));
+		SEARCH_QUEUE.add(new Page(baseUrl, downloader.downloadPage(baseUrl)));
 		do {
 			Page page = SEARCH_QUEUE.poll();
-			logger.info("Searching for new links in page " + page.url());
+			logger.info("Searching for new links in page " + page.getUrl());
 
 			DOWNLOADED_PAGES.add(page);
-			String contents = page.contents();
+			String contents = page.getContents();
 
 			ANCHOR.matcher(contents) // Search for the HTML anchor elements in a page
 					.results() // Get a Stream with the regex matches
 					.map(MatchResult::group) // Get the matching Strings
-					.filter(SearchUtils::containsHref) // Remove the elements that do not contain the field href
-					.map(SearchUtils::extractHref) // Extract the link contained in the href
-					.filter(page::isValidLink) // Remove links that lead to other pages
-					.map(page::mapToAbsoluteLink) // Generate the absolute links from the relative links
-					.map(DownloadProcess::checkVisitedLinks)
-					.filter(link -> !link.isBlank())
-					.map(Page::new) // Create a new Page object and start the download process
-					.forEach(SEARCH_QUEUE::add); // Add these new pages for the next step of the search
+					.filter(this::containsHref).map(this::extractHref).filter(page.startsWithValidCharacter)
+					.map(page::mapToAbsoluteLink).map(DownloadProcess::checkVisitedLinks).filter(Objects::nonNull)
+					.map(link -> new Page(link, downloader.downloadPage(contents))).forEach(SEARCH_QUEUE::add);
 
 		} while (!SEARCH_QUEUE.isEmpty());
 		logger.info("Download Process complete!");
@@ -88,11 +91,29 @@ public final class DownloadProcess implements Runnable {
 		return VISITED_PAGES;
 	}
 
+	/**
+	 * If the link has already been visited, make it a null, otherwise, add it to
+	 * the list and return it.
+	 */
 	private static String checkVisitedLinks(String string) {
 		if (VISITED_PAGES.contains(string)) {
-			return "";
+			return null;
 		}
 		VISITED_PAGES.add(string);
 		return string;
+	}
+
+	private boolean containsHref(String string) {
+		return string.contains(HREF);
+	}
+
+	/**
+	 * Extract the href from the anchor element. This method assumes that the href
+	 * is the first quoted element to appear on anchor element.
+	 */
+	private String extractHref(String element) {
+		int start = element.indexOf(HREF) + HREF.length();
+		int end = element.indexOf("\"", start);
+		return element.substring(start, end);
 	}
 }
