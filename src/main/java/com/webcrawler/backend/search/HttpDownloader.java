2b49eb6bb2fffe0c773e974c8b8bdbded24627b8
Gabriel Oliveira <santosgabriel28@hotmail.com>
2024-01-06 15:05:56 -0300

    formatting changes, removing the static modifier from some methods

--- src/main/java/com/webcrawler/backend/search/HttpDownloader.java
+++ src/main/java/com/webcrawler/backend/search/HttpDownloader.java
@@ -1,8 +1,5 @@
 package com.webcrawler.backend.search;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpClient.Version;
@@ -12,26 +9,24 @@ import java.net.http.HttpResponse.BodyHandlers;
 import java.time.Duration;
 import java.util.concurrent.CompletableFuture;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public final class HttpDownloader {
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(HttpDownloader.class);
-	
-	private static final HttpClient CLIENT = HttpClient.newBuilder()
-			.version(Version.HTTP_2)
-			.connectTimeout(Duration.ofSeconds(10))
-			.build();
-	
-	public static CompletableFuture<String> downloadPage(String url) {
-		HttpRequest request = HttpRequest.newBuilder(URI.create(url))
-				.timeout(Duration.ofSeconds(5L))
-				.build();
-		
-		return CLIENT.sendAsync(request, BodyHandlers.ofString())
-				.thenApply(HttpResponse::body)
+
+	private static final HttpClient CLIENT = HttpClient.newBuilder().version(Version.HTTP_2)
+			.connectTimeout(Duration.ofSeconds(10)).build();
+
+	public CompletableFuture<String> downloadPage(String url) {
+		HttpRequest request = HttpRequest.newBuilder(URI.create(url)).timeout(Duration.ofSeconds(5L)).build();
+
+		return CLIENT.sendAsync(request, BodyHandlers.ofString()).thenApply(HttpResponse::body)
 				.exceptionally(throwable -> logError(throwable, url));
 	}
 
-	private static String logError(Throwable t, String page) {
+	private String logError(Throwable t, String page) {
 		logger.error("Error while downloading page {}\n Error: {}", page, t.getMessage());
 		return "";
 	}
